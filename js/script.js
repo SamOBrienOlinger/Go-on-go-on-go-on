136ecd1d211ed35525ede1ac21b786b50381c944
SamOBrienOlinger <samtobo@yahoo.co.uk>
2022-05-20 12:25:52 +0000

    test function

--- js/script.js
+++ js/script.js
@@ -4,6 +4,14 @@ let arrayNames = ["Sam", "Aline", "Benji"];
 
 let arrayActions = ["stink", "eat", "poop", "play", "hide", "sniff","run"];
 
+function findRandomWord(listOfWords) {
 
-let random = [Math.floor(Math.random()*arrayNumbers.length)];
+    let random = [Math.floor(Math.random()*listOfWords.length)];
 
+    return listOfWords[random]
+}
+
+
+console.log(findRandomWord(arrayNumbers));
+console.log(findRandomWord(arrayNames));
+console.log(findRandomWord(arrayActions));
