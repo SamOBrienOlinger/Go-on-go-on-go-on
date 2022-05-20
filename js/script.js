501312b1ec2328b9f99d1e829a9e9bdab29e8b0e
SamOBrienOlinger <samtobo@yahoo.co.uk>
2022-05-20 12:08:36 +0000

    add three lists of arrays

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let arrayNumbers = ["zero", "one", "two", "three", "four", "five","six", "seven", "eight", "nine", "ten", "eleven", "twelve"];
+
+let arrayNames = ["Sam", "Aline", "Benji"];
+
+let arrayActions = ["stink", "eat", "poop", "play", "hide", "sniff","run"];
+
+
+let random = [Math.floor(Math.random()*arrayNumbers.length)];
+
